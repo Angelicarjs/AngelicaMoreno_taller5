4db06fb5b1e7d805ae90051031dff51a4790e422
Angelicarjs <37352302+Angelicarjs@users.noreply.github.com>
2018-05-14 08:15:14 -0500

    Add files via upload

---
+++ punto_2/cuerda.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <math.h>
+
+using namespace std;
+
+// Declaracion de variables
+
+float p;
+float T;
+float L;
+float c = pow(T/p,(1/2));
+
+//main
+int main(){
+	return 0;
+} 
